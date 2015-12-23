70e37ebb1fe5a13d9b45e2ca58581ef63318d471
Rahul Yadav <rahul.yadav@indpro.se>
2015-12-23 18:19:41 +0530

    CircleSpeedDial class added

---
+++ app/src/main/java/com/floating/example/test/controller/CircleSpeedDial.java
@@ -0,0 +1,137 @@
+package com.floating.example.test.controller;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.AbsoluteLayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by iprahul on 12/23/15.
+ */
+public class CircleSpeedDial extends AbsoluteLayout implements View.OnClickListener, ViewTreeObserver.OnPreDrawListener {
+    private static final String TRANSLATION_Y = "translationY";
+    private ViewGroup viewGroup;
+    private boolean expanded = false;
+    private FloatingActionButton fab;
+    private ExpandListener expandListener;
+
+    private HashMap<View, Float> viewMap = new HashMap<>();
+
+    public CircleSpeedDial(Context context) {
+        super(context);
+    }
+
+    public CircleSpeedDial(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleSpeedDial(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        expanded = !expanded;
+        if (expanded) {
+            expandFab();
+        } else {
+            collapseFab();
+        }
+    }
+
+    public void setExpandListener(ExpandListener expandListener) {
+        this.expandListener = expandListener;
+    }
+
+    private Animator createCollapseAnimator(View view, float offset) {
+        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, 0, offset)
+                .setDuration(view.getContext().getResources().getInteger(android.R.integer.config_mediumAnimTime));
+    }
+
+    private Animator createExpandAnimator(View view, float offset) {
+        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, offset, 0)
+                .setDuration(view.getContext().getResources().getInteger(android.R.integer.config_mediumAnimTime));
+    }
+
+    private void collapseFab() {
+        if (expandListener != null) {
+            expandListener.onCollapse(fab);
+        }
+        AnimatorSet animatorSet = new AnimatorSet();
+        List<Animator> animators = new ArrayList<>();
+        for (View view : viewMap.keySet()) {
+            animators.add(createCollapseAnimator(view, viewMap.get(view)));
+        }
+        animatorSet.playTogether(animators);
+        animatorSet.start();
+        animateFab();
+    }
+
+    private void expandFab() {
+        if (expandListener != null) {
+            expandListener.onExpand(fab);
+        }
+        AnimatorSet animatorSet = new AnimatorSet();
+        List<Animator> animators = new ArrayList<>();
+        for (View view : viewMap.keySet()) {
+            animators.add(createExpandAnimator(view, viewMap.get(view)));
+        }
+        animatorSet.playTogether(animators);
+        animatorSet.start();
+        animateFab();
+    }
+
+    private void animateFab() {
+        Drawable drawable = fab.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+    }
+
+    @Override
+    public boolean onPreDraw() {
+        viewGroup.getViewTreeObserver().removeOnPreDrawListener(this);
+        float offset = 0;
+        for (View view : viewMap.keySet()) {
+            offset = fab.getY() - view.getY();
+            viewMap.put(view, offset);
+            view.setTranslationY(offset);
+        }
+        return true;
+    }
+
+    public FloatingActionButton getFab() {
+        return fab;
+    }
+
+    public void setOnItemClickListener(View.OnClickListener onItemClickListener) {
+        if (onItemClickListener != null) {
+            for (View view : viewMap.keySet()) {
+                view.setOnClickListener(onItemClickListener);
+            }
+        }
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    public static interface ExpandListener {
+        public void onExpand(FloatingActionButton button);
+
+        public void onCollapse(FloatingActionButton button);
+    }
+}
